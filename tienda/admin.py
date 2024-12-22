65394412ee1171b087a1301c28e4174ceadd4995
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2024-12-21 23:27:07 -0500

    Agregando los modelos al panel de administracion

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CategoriaProduct,Producto
 
 # Register your models here.
+
+class CategoriaProductAdmin(admin.ModelAdmin):
+
+    readonly_fields=("created","updated")
+
+class ProductAdmin(admin.ModelAdmin):
+
+    readonly_fields=("created","updated")
+
+admin.site.register(CategoriaProduct, CategoriaProductAdmin)
+admin.site.register(Producto, ProductAdmin)
