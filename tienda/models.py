76e0e93cb2fd1db592579b636818a551e5040851
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2024-12-21 19:06:09 -0500

    Creando los modelos de la app tienda

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class CategoriaProduct(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name="categoriaProduct"
+        verbose_name_="categoriaProducts"
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    
+    nombre = models.CharField(max_length=50)
+    categorias = models.ForeignKey(CategoriaProduct, on_delete=models.CASCADE)   
+    imagen = models.ImageField(upload_to="tienda", null=True, blank=True)
+    precio = models.FloatField()
+    disponibilidad = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name="Producto"
+        verbose_name_="Productos"
