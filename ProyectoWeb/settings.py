fa7e0876a1f8caa96f743bae0d9a1593720b5f2a
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2024-12-22 12:44:30 -0500

    Trabajando en el Template de tienda ya los productos se ven imagen, nombre y precio

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rojassucarinomiguelantonio@gmail.com'
-EMAIL_HOST_PASSWORD = 'Migueldev.2025'
+EMAIL_HOST_USER = '[Usuario de Correo]'
+EMAIL_HOST_PASSWORD = '[Password del Correo]'
