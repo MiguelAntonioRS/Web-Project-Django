ac830e9cef949147ee9d335458a55f5b1d39dc14
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-08 12:21:43 -0500

    Comprobando si hay o no Carros (logica)

--- carro/carro.py
+++ carro/carro.py
@@ -8,3 +8,13 @@ class Carro:
             carro = self.session["carro"] = {}
         else:
             self.carro = carro    
+
+    def agregar(self, producto):
+        if(str(producto.id) not in self.carro.keys()):
+            self.carro[producto.id]={
+                "producto_id":producto.id,
+                "nombre":producto.nombre,
+                "precio":str(producto.precio),
+                "cantidad":1,
+                "imagen":producto.imagen.url
+            }
