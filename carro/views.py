02f4fad9d622ce032d7226bb887f4bbe8a48fb81
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-24 18:04:16 -0500

    Creando funcion agregar en las views

--- carro/views.py
+++ carro/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .carro import Carro
+from tienda.models import Producto
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def agregar_producto(request, producto_id):
+    
+    carro = Carro(request)
+    
+    producto = Producto.objects.get(id=producto_id)
+    
+    carro.agregar(producto=producto)
+    
+    return redirect("tienda")
